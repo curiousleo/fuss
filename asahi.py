39b0605d158b53ff62a95b8c460b2eb483f4c0f3
Leonhard Markert <curiousleo@ymail.com>
2015-11-30 20:42:52 +0000

    Author identification and more

--- asahi.py
+++ asahi.py
@@ -1,3 +1,4 @@
+import re
 from time import strptime
 from urllib.parse import urlencode
 
@@ -5,35 +6,67 @@ from bs4 import BeautifulSoup
 import requests
 
 import fuss
+from wire import iswire
 
 BASE_URL = 'https://ajw.asahi.com'
 DATE_FORMAT = '%B %d, %Y'
+REGEXES = {
+    re.compile(r'^B. ([A-Za-z ]+)/ Staff Writer$'): fuss.Author.staff,
+    re.compile(r'^B. ([A-Za-z ]+)/ Senior Staff Writer$'): (lambda name: fuss.Author.staff(name, 'senior')),
+    re.compile(r'^B. ([A-Za-z ]+)/ Correspondent$'): (lambda name: fuss.Author.staff(name, 'correspondent')),
+    re.compile(r'^B. ([A-Za-z ]+)$'): fuss.Author.guest,
+}
+ASAHI_REGEX = re.compile(r'^THE AS(A)?HI SHIMBUN$')
+VOX_REGEX = re.compile(r'^Vox Populi')
 
 class Article(fuss.Article):
 
-    def __init__(self, url, session = None):
-        super().__init__(url)
+    def __init__(self, *args):
+        super().__init__(*args)
 
     @property
     def date(self):
-        date = self._soup.select('#article .date')[0]
-        return strptime(date.get_text(), DATE_FORMAT)
+        selector = '#article .date, #article_wrap .article_date'
+        date = self._soup.select(selector)[0]
+        return strptime(date.get_text().strip(), DATE_FORMAT)
 
     @property
     def author(self):
-        author = self._soup.select('.author')[0]
-        return author.get_text()[3:].split('/')[0].title()
+        selection = self._soup.select('.author')
+        if len(selection) > 0:
+            return parse_author(selection[0].get_text().strip())
+        else:
+            return fuss.Author.unknown()
 
     @property
     def title(self):
-        return self._soup.select('#article_head h1')[0].get_text()
+        selector = '#article_head h1, #article_wrap .article_title'
+        return self._soup.select(selector)[0].get_text().strip()
 
     @property
     def text(self):
-        ps = self._soup.select('.text')[0]
+        ps = self._soup.select('.text, #article_wrap .article_summary')[0]
         return ''.join(ps.findAll(text = True)).strip()
 
+    @property
+    def photos(self):
+        selector = '#article .media_c img, #article .photo_list li, #article_wrap .leaf img'
+        return len(self._soup.select(selector))
+
 fuss.Article.register(Article)
+
+def parse_author(string):
+    if ASAHI_REGEX.search(string):
+        return fuss.Author.staff()
+    if VOX_REGEX.search(string):
+        return fuss.Author.staff('Vox Populi')
+    for regex, author in REGEXES.items():
+        s = regex.search(string)
+        if s:
+            return author(s.groups(0)[0].title())
+    if iswire(string):
+        return fuss.Author.wire(string.title())
+    return fuss.Author.unknown(string)
 
 def find(keywords):
     s = requests.Session()
